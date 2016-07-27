90c77eeff7428fa4340638dd51d935fa37e0a8d3
Ryan Munger <ryanmunger@gmail.com>
2016-07-27 16:39:43 -0700

    Refactor webpack config

--- server.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-const server = new WebpackDevServer(compiler, {});
-
-server.listen(8080);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
-const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080/',
-        'webpack/hot/dev-server',
-        './src/index'
-    ],
+    context: path.join(__dirname, 'src'),
+    devtool: 'inline-sourcemap',
+    entry: './index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
+            }
+        ]
+    },
     output: {
+        path: __dirname + '/src/',
         filename: 'bundle.js'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        loaders: []
-    },
-    resolve: {
-        root: path.resolve('./src')
-    }
+    plugins: []
 };
